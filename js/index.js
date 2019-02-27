82fd880557a2d50a69f7dfef9fdd2abdb6a55d35
辛洪伍 <1872707806@qq.com>
2019-02-27 20:03:05 +0800

    bootstrap实现响应式布局

---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(function() {
+    //获取工具提示的元素 初始化工具提示插件
+    // 默认没有初始化需要手动初始化
+    $('[data-toggle="tooltip"]').tooltip();
+
+    /* 实现手势滑动切换的上一张和下一张
+        1. 知道当前滑动的方向
+        2. 从左往右滑  切换到上一张 prev  计算滑动的距离正表示从左往右
+        3. 从右往左滑  切换到下一张 next  计算滑动距离如果是负值就表示从右往左 */
+        var startX = 0;
+        var endX = 0;
+        $('#slide').on('touchstart',function (e){     
+            // jquery添加的事件 事件对象是jquery的事件对象
+            // 取里面原生事件对象e.originalEvent
+            // console.log(e);
+            // console.log(e.originalEvent);
+            startX = e.originalEvent.touches[0].clientX;
+            console.log(startX);
+        });
+        $('#slide').on('touchend',function (e){        
+            // console.log(e);
+            // console.log(e.originalEvent);
+            // 结束事件只能使用changedTouches 来获取手指位置
+            endX = e.originalEvent.changedTouches[0].clientX;
+            console.log(endX);
+            // 判断endX - startX的值 如果是正值 从左往右 就切换到上一张
+            if(endX - startX > 0){
+                $('.carousel').carousel('prev')
+            }else if(endX - startX < 0){// 当小于0的时候 从右往左 切换到下一张
+                $('.carousel').carousel('next')
+            }
+        });
+})
